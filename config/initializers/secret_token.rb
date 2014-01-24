1a2f4fdb4982f56cf5f844abdefc368b594e1437
Craig Wermert <craig.wermert@osumc.edu>
2014-01-24 07:43:51 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmugSaints::Application.config.secret_key_base = '04e65484c23f5144366de347b5c42804f35fad73baacf7feaf979fcc7de88bfb9401c64631ff73dd58370d939c87cd100451ffc784989e5ca0a27e928b8636fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SmugSaints::Application.config.session_store :cookie_store, key: '_smug_saints_session'
